a17a11a99e0e84c5acc4eb02142f727ceef8e4bd
Somphoph <somphop.homprasert@krungthai.com>
2022-01-17 18:22:43 +0700

    Initial project

---
+++ src/main/java/com/example/gitlabtool/GitLabToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlabtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLabToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLabToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitlabtool/GitLabToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitlabtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLabToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
